3d4352e89fc3bbf14a92e659a13ace9ae29f89a6
yPedroDev <129919473+yPedroDev@users.noreply.github.com>
2023-04-29 09:02:29 -1200

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -13,7 +13,7 @@ module.exports = (app) => {
   );
   app.get("/api/succeso", (req, res) => {
     if(req.user){
-     res.sendStatus('OK');
+     res.sendStatus(200);
     }else{
      res.send("NOT LOGGED IN");
     }
