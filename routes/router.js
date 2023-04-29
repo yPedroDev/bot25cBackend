4310f43ca3f5c304ad697d1c86ae9a955fb08e32
yPedroDev <129919473+yPedroDev@users.noreply.github.com>
2023-04-29 04:03:36 -1200

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -19,7 +19,7 @@ module.exports = (app) => {
       if (err) {
         return next(err);
       }
-      res.redirect("/");
+      res.redirect("https://bot25c.vercel.app/");
     });
   });
   app.get(
