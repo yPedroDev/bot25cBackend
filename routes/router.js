dffe820a3f736a1326f405c1546428195146ca8b
yPedroDev <129919473+yPedroDev@users.noreply.github.com>
2023-04-30 06:47:43 -1200

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -11,7 +11,7 @@ module.exports = (app) => {
       res.send(200);
     }
   );
-  app.get("/api/succeso", (req, res) => {
+  app.post("/api/succeso", (req, res) => {
     try {
       const { username, locale } = req.user;
       const user = ({
